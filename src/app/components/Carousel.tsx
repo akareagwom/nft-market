46f6fcab9b055e26ef11bf1d3eae0d5365a451f2
akareagwom <akareazikang20@gmail.com>
2025-01-30 09:13:51 +0100

    carousel update2

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -6,9 +6,23 @@ interface Props {
 }
 
 const Carousel: React.FC<Props> = ({  }) => {
+
+    const info = [
+        { time:'2:41 min left', price: "2.55 ETH", content: 'Tristique diam a, enim, eros tellus. Viverra etiam', image:'/assets/Image.png', people:'14 people are bidding', src: "https://cdn.myanimelist.net/r/84x124/images/characters/9/131317.webp?s=d4b03c7291407bde303bc0758047f6bd",
+            name: "Uchiha Sasuke", },
+        { time:'22:59 min left', price: "3.19 ETH", content: 'Vulputate felis purus viverra morbi facilisi eget', image:'/assets/Image1.png', people:'35 people are bidding' , src: "https://cdn.myanimelist.net/r/84x124/images/characters/9/131317.webp?s=d4b03c7291407bde303bc0758047f6bd",
+            name: "Uchiha Sasuke",},
+        { time:'2:41 min left', price: "1.11 ETH", content: 'Dui accumsan leo vestibulum ornare eu', image:'/assets/Image2.png', people:'101 people are bidding' , src: "https://cdn.myanimelist.net/r/84x124/images/characters/9/131317.webp?s=d4b03c7291407bde303bc0758047f6bd",
+            name: "Uchiha Sasuke",},
+        { time:'37:01 min left', price: "1.63 ETH", content: 'Senectus adipiscing nascetur accumsan etiam', image:'/assets/Image3.png', people:'12 people are bidding',  src: "https://cdn.myanimelist.net/r/84x124/images/characters/9/131317.webp?s=d4b03c7291407bde303bc0758047f6bd",
+            name: "Uchiha Sasuke", },
+        { time:'2:41 min left', price: "2.55 ETH", content: 'Mattis at diam lorem nisl nam sed sociis', image:'/assets/Image4.png', people:'19 people are bidding',  src: "https://cdn.myanimelist.net/r/84x124/images/characters/9/131317.webp?s=d4b03c7291407bde303bc0758047f6bd",
+            name: "Uchiha Sasuke", },
+    ];
+
   return (
-    <Flex>
-      <Card.Root border='1px solid grey' color={'white'} borderRadius={'10px'} px={2} mx={4} w={'300px'} bg='transparent'>
+    <Flex alignItems={'baseline'} justifyContent={'center'} w='100%'>
+      <Card.Root py={4} border='1px solid grey' color={'white'} borderRadius={'10px'} px={2} mx={4} w={'300px'} bg='transparent'>
         <Card.Header fontSize={'3xl'}>Check out the hottest Sale offers</Card.Header>
         <Card.Body>
             <Flex>
@@ -41,7 +55,7 @@ const Carousel: React.FC<Props> = ({  }) => {
                     </Card.Body>
                 </Card.Root>
             </Flex>
-            <Button>Show me more</Button>
+            <Button bg='transparent' border={'0.5px solid grey'} borderRadius={'10px'}>Show me more</Button>
         </Card.Body>
       </Card.Root>
 
@@ -62,6 +76,35 @@ const Carousel: React.FC<Props> = ({  }) => {
                 </Card.Root>
             </Box>
 
+            {/* box 3 */}
+                <Card.Root py={4} bg={'transparent'} color={'white'} borderRadius={'10px'} px={2} mx={4} w={'300px'} >
+                    <Card.Header>
+                        Top NFT at a lower price
+                    </Card.Header>
+                    <Card.Body>
+                        
+                                    
+                                    {info.map((inform)=>(
+                                        <Box display={'flex'} justifyContent={'space-between'} p={2} alignItems={'center'} key={inform.price}>
+                                        <Image my={2} w={'200'} h={'100px'}  src={inform.image}
+                                                borderRadius={'10px'}/>
+                                    <Box w='60%'>
+
+                                            <Text fontSize="14px">{inform.content}</Text>
+                                        <Flex justifyContent={'space-between'}>
+                                            <Flex>
+                                                <RiTimerLine />
+                                                <Text fontSize="10px">{inform.time}</Text>
+                                            </Flex>
+                                                <Button bg="#514CFF26" color="#514CFF" fontSize="10px">{inform.price}</Button>
+                                        </Flex>
+                                    </Box>
+
+                            </Box>
+                ))}
+                <Button bg='transparent' border={'0.5px solid grey'} borderRadius={'10px'}>Show me more</Button>
+                    </Card.Body>
+                </Card.Root>
     </Flex>
   );
 };
